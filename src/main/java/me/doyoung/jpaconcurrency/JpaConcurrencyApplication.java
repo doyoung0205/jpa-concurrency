85f88962d2998e51945848416b898baa9318d8d5
김도영 <doyoung0205@naver.com>
2022-02-09 22:26:00 +0900

    Initial commit

---
+++ src/main/java/me/doyoung/jpaconcurrency/JpaConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package me.doyoung.jpaconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaConcurrencyApplication.class, args);
+    }
+
+}
