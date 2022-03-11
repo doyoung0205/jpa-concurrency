cdc5d5cf8a583d44c8d284998d546b9085de6ad2
김도영 <doyoung0205@naver.com>
2022-03-11 20:43:57 +0900

    refactor : controller log 추가

--- src/main/java/me/doyoung/jpaconcurrency/reservation/ui/ReservationController.java
+++ src/main/java/me/doyoung/jpaconcurrency/reservation/ui/ReservationController.java
@@ -1,6 +1,7 @@
 package me.doyoung.jpaconcurrency.reservation.ui;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import me.doyoung.jpaconcurrency.reservation.application.ReservationService;
 import me.doyoung.jpaconcurrency.reservation.dto.ReservationDtos;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/reserve")
@@ -19,6 +21,9 @@ public class ReservationController {
     @PostMapping
     public ResponseEntity<ReservationDtos.Response> reserve(
             @RequestBody ReservationDtos.Request request) {
-        return ResponseEntity.ok().body(service.reserve(request));
+        log.info("{} 예약 신청 controller 시작", Thread.currentThread().getName());
+        final ResponseEntity<ReservationDtos.Response> result = ResponseEntity.ok().body(service.reserve(request));
+        log.info("{} 예약 신청 controller 종료", Thread.currentThread().getName());
+        return result;
     }
 }
